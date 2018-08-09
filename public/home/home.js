222953f9523642045dbc1c5506d1bc3bd5737f31
Darren Nelsen <darren@bitpay.com>
2018-08-09 17:51:07 -0400

    use typescript

--- public/home/home.js
+++ public/home/home.js
@@ -1,43 +1,37 @@
 angular.module('app').component('home', {
-  templateUrl: 'home/home.html',
-  bindings: {
-    userSessions: '='
-  },
-  controller: function(currentIdentity, sessions,
-    toastr, unreviewedSessionCount) {
-
-
-    this.currentUser = currentIdentity.currentUser
-
-    this.setNextSessionToReview = function() {
-      sessions.getNextUnreviewedSession(currentIdentity.currentUser.id).then(function(response) {
-        this.currentSessionToReview = response.data;
-      }.bind(this))
+    templateUrl: 'home/home.html',
+    bindings: {
+        userSessions: '='
+    },
+    controller: function (currentIdentity, sessions, toastr, unreviewedSessionCount) {
+        this.currentUser = currentIdentity.currentUser;
+        this.setNextSessionToReview = function () {
+            var _this = this;
+            sessions.getNextUnreviewedSession(currentIdentity.currentUser.id).then(function (response) {
+                _this.currentSessionToReview = response.data;
+            });
+        };
+        this.setNextSessionToReview();
+        this.voteYes = function () {
+            var _this = this;
+            sessions.incrementVote(this.currentSessionToReview.id)
+                .then(function () {
+                sessions.addReviewedSession(_this.currentUser.id, _this.currentSessionToReview.id);
+            })
+                .then(function () {
+                _this.setNextSessionToReview();
+                // pull updated value
+                unreviewedSessionCount.updateUnreviewedSessionCount();
+            });
+        };
+        this.voteNo = function () {
+            sessions.addReviewedSession(this.currentUser.id, this.currentSessionToReview.id)
+                .then(function () {
+                this.setNextSessionToReview();
+                // pull updated value
+                unreviewedSessionCount.updateUnreviewedSessionCount();
+            }.bind(this));
+        };
     }
-    this.setNextSessionToReview();
-
-
-    this.voteYes = function() {
-      sessions.incrementVote(this.currentSessionToReview.id)
-        .then(function() {
-          return sessions.addReviewedSession(this.currentUser.id, this.currentSessionToReview.id)
-        }.bind(this))
-        .then(function() {
-          this.setNextSessionToReview();
-
-          // pull updated value
-          unreviewedSessionCount.updateUnreviewedSessionCount();
-        }.bind(this))
-    }
-
-    this.voteNo = function() {
-      sessions.addReviewedSession(this.currentUser.id, this.currentSessionToReview.id)
-        .then(function() {
-          this.setNextSessionToReview();
-
-          // pull updated value
-          unreviewedSessionCount.updateUnreviewedSessionCount();
-        }.bind(this))
-    }
-  }
-})
+});
+//# sourceMappingURL=home.js.map
---
+++ public/home/home.ts
@@ -0,0 +1,43 @@
+angular.module('app').component('home', {
+  templateUrl: 'home/home.html',
+  bindings: {
+    userSessions: '='
+  },
+  controller: function(currentIdentity, sessions,
+    toastr, unreviewedSessionCount) {
+
+
+    this.currentUser = currentIdentity.currentUser
+
+    this.setNextSessionToReview = function() {
+      sessions.getNextUnreviewedSession(currentIdentity.currentUser.id).then((response) => {
+        this.currentSessionToReview = response.data;
+      });
+    }
+    this.setNextSessionToReview();
+
+
+    this.voteYes = function() {
+      sessions.incrementVote(this.currentSessionToReview.id)
+        .then(() => {
+          sessions.addReviewedSession(this.currentUser.id, this.currentSessionToReview.id)
+        })
+        .then(() => {
+          this.setNextSessionToReview();
+
+          // pull updated value
+          unreviewedSessionCount.updateUnreviewedSessionCount();
+        });
+    }
+
+    this.voteNo = function() {
+      sessions.addReviewedSession(this.currentUser.id, this.currentSessionToReview.id)
+        .then(function() {
+          this.setNextSessionToReview();
+
+          // pull updated value
+          unreviewedSessionCount.updateUnreviewedSessionCount();
+        }.bind(this))
+    }
+  }
+})
