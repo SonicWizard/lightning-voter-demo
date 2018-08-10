c597bb398034a446ef7be88547d939948ffd4af2
Darren Nelsen <darren@bitpay.com>
2018-08-10 12:22:42 -0400

    convert currentIdentity service to class

---
+++ public/security/currentIdentity.ts
@@ -0,0 +1,31 @@
+angular.module('app').service('currentIdentity', class CurrentIdentity {
+  currentUser;
+
+  constructor(private $http, private $q) {
+  }
+
+  setUser(user) {
+    this.currentUser = user;
+  }
+
+  clearUser() {
+    this.currentUser = null;
+  }
+
+  authenticated() {
+    return !!this.currentUser;
+  }
+
+  updateUser(newUserObj) {
+    let dfd = this.$q.defer();
+
+    this.$http.put(`/api/users/${this.currentUser.id}`, newUserObj).then((response) => {
+      this.currentUser.firstName = newUserObj.firstName;
+      this.currentUser.lastName = newUserObj.lastName;
+      dfd.resolve();
+    }, (response) => {
+      dfd.reject("Error Logging Out");
+    })
+    return dfd.promise;
+  }
+});
